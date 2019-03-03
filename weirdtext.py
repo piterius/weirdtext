3a7e63efdcdfe397f99d244cf2d3b9d0ffaa3351
piterius <edgar.pilch@protonmail.ch>
2019-03-04 00:15:40 +0100

    refactoring weirdtext

--- weirdtext.py
+++ weirdtext.py
@@ -4,34 +4,33 @@ import random
 
 def encode(sentence, separator='\n---weird---\n'):
     original_words = list()
-    sentence_lst = list(sentence)
+    sentence_letters = list(sentence)
     words = re.finditer(r'\w+', sentence)
     for word in words:
         if len(word.group(0)) > 3:
-            original = sentence_lst[word.start()+1:word.end()-1]
+            original = sentence_letters[word.start() + 1:word.end() - 1]
             check = {letter for letter in original}
             if len(check) > 1:
                 changed = original[:]
                 while changed == original:
                     random.shuffle(changed)
-                sentence_lst[word.start()+1:word.end()-1] = changed
-                original_words.append(sentence[word.start():word.end()])
-    return separator + "".join(sentence_lst) + separator + " ".join(sorted(original_words, key=str.casefold))
+                sentence_letters[word.start() + 1:word.end() - 1] = changed
+                original_words.append(word.group(0))
+    return separator + "".join(sentence_letters) + separator + " ".join(sorted(original_words, key=str.casefold))
 
 
 def decode(sentence, separator='\n---weird---\n'):
     _, text, original_words = sentence.split(separator)
-    text_lst = list(text)
+    text_letters = list(text)
     original_words = original_words.split(" ")
-    original_words_dict = {i: sorted([letter for letter in original_words[i]]) for i in range(len(original_words))}
+    original_words_as_letters = {i: sorted(list(word)) for i, word in enumerate(original_words)}
     words = re.finditer(r'\w+', text)
     for word in words:
         if len(word.group(0)) > 3:
             changed_word = sorted(list(word.group(0)))
-            for i, original_word in original_words_dict.items():
+            for i, original_word in original_words_as_letters.items():
                 if changed_word == original_word:
-                    text_lst[word.start():word.end()] = original_words[i]
-                    del(original_words_dict[i])
+                    text_letters[word.start():word.end()] = original_words[i]
+                    del original_words_as_letters[i]
                     break
-    return "".join(text_lst)
-
+    return "".join(text_letters)
